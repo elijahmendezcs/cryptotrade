9b7ec118920c532846ea50e5e634edd9f3f397ee
elijahmendezcs <elijahmendez2022@gmail.com>
2025-05-26 22:20:26 -0400

    dev branch commit

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,6 @@
 // frontend/src/app/layout.tsx
+
+// THIS IS WHERE THE ROUTING HAPPENS 
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
